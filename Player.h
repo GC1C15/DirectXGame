5b8142e69349073d21033651ac7b724103a001a3
GC1C15 <k022g1077@g.neec.ac.jp>
2023-07-06 16:20:19 +0900

    変更

--- Player.h
+++ Player.h
@@ -1,2 +1,33 @@
-#pragma once
-class Player {};
+﻿#pragma once
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "MathUtilityForText.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+class Player {
+public:
+	// コンストラクタ
+	Player();
+	// デストラクタ
+	~Player();
+
+	// 初期化
+	void Initialize(ViewProjection viewProjection);
+	// 更新
+	void Update();
+	// 3D描画
+	void Draw3D();
+	// インプットクラス
+	Input* input_ = nullptr;
+
+private:
+	ViewProjection viewProjection_;
+	// プレイヤー
+	uint32_t textureHandlePlayer_ = 0;
+	Model* modelPlayer_ = nullptr;
+	WorldTransform worldTransformPlayer_;
+};
