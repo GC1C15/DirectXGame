7fbf201f9d5064e2d788aaf44b73fad03ee0bf44
GC1C15 <k022g1077@g.neec.ac.jp>
2023-07-06 16:47:25 +0900

    変更

--- Beam.cpp
+++ Beam.cpp
@@ -1,1 +1,36 @@
 #include "Beam.h"
+
+Beam::Beam() {}
+
+Beam::~Beam() { delete modelBeam_; }
+
+void Beam::Initialize(ViewProjection viewProjection) {
+	viewProjection_ = viewProjection;
+	// プレイヤー
+	textureHandleBeam_ = TextureManager::Load("player.png");
+	modelBeam_ = Model::Create();
+	textureHandleBeam_.scale_ = {0.5f, 0.5f, 0.5f};
+	worldTransformBeam_.Initialize();
+	// インプットクラス
+	input_ = Input::GetInstance();
+}
+
+void Beam::Update() {
+	// 変更行列の更新
+	textureHandleBeam_.matWorld_ = MakeAffineMatrix(
+	    worldTransformBeam_.scale_, worldTransformBeam_.rotation_,
+	    worldTransformBeam_.translation_);
+	// 変更行列の定数バッファを転送
+	worldTransformBeam_.TransferMatrix();
+	if (worldTransformBeam_.translation_.x > 4) {
+		worldTransformBeam_.translation_.x = 4;
+	}
+	if (worldTransformBeam_.translation_.x < -4) {
+		worldTransformBeam_.translation_.x = -4;
+	}
+}
+
+void Beam::Draw3D() {
+	// stage
+	modelBeam_->Draw(worldTransformBeam_, viewProjection_, textureHandleBeam_);
+}
--- Beam.h
+++ Beam.h
@@ -1,2 +1,33 @@
 #pragma once
-class Beam {};
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "MathUtilityForText.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+class Beam {
+public:
+	// コンストラクタ
+	Beam();
+	// デストラクタ
+	~Beam();
+
+	// 初期化
+	void Initialize(ViewProjection viewProjection);
+	// 更新
+	void Update();
+	// 3D描画
+	void Draw3D();
+	// インプットクラス
+	Input* input_ = nullptr;
+
+private:
+	ViewProjection viewProjection_;
+	// ビーム
+	uint32_t textureHandleBeam_ = 0;
+	Model* modelBeam_ = nullptr;
+	WorldTransform worldTransformBeam_;
+};
