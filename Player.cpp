5290b30907672260f72af7a7e3a84ca7dfdc8f01
GC1C15 <k022g1077@g.neec.ac.jp>
2023-07-14 10:47:03 +0900

    プレイヤークラスの追加

---
+++ Player.cpp
@@ -0,0 +1,42 @@
+﻿#include "Player.h"
+
+Player::Player() {}
+
+Player::~Player() { delete modelPlayer_; }
+
+void Player::Initialize(ViewProjection viewProjection) {
+	viewProjection_ = viewProjection;
+	// プレイヤー
+	textureHandlePlayer_ = TextureManager::Load("player.png");
+	modelPlayer_ = Model::Create();
+	worldTransformPlayer_.scale_ = {0.5f, 0.5f, 0.5f};
+	worldTransformPlayer_.Initialize();
+	// インプットクラス
+	input_ = Input::GetInstance();
+}
+
+void Player::Update() {
+	// 変更行列の更新
+	worldTransformPlayer_.matWorld_ = MakeAffineMatrix(
+	    worldTransformPlayer_.scale_, worldTransformPlayer_.rotation_,
+	    worldTransformPlayer_.translation_);
+	// 変更行列の定数バッファを転送
+	worldTransformPlayer_.TransferMatrix();
+	if (input_->PushKey(DIK_RIGHT)) {
+		worldTransformPlayer_.translation_.x += 0.1f;
+	}
+	if (input_->PushKey(DIK_LEFT)) {
+		worldTransformPlayer_.translation_.x -= 0.1f;
+	}
+	if (worldTransformPlayer_.translation_.x > 4) {
+		worldTransformPlayer_.translation_.x = 4;
+	}
+	if (worldTransformPlayer_.translation_.x < -4) {
+		worldTransformPlayer_.translation_.x = -4;
+	}
+}
+
+void Player::Draw3D() {
+	// stage
+	modelPlayer_->Draw(worldTransformPlayer_, viewProjection_, textureHandlePlayer_);
+}
---
+++ Player.h
@@ -0,0 +1,36 @@
+﻿﻿#pragma once
+#include "DirectXCommon.h"
+#include "Input.h"
+#include "MathUtilityForText.h"
+#include "Model.h"
+#include "SafeDelete.h"
+#include "Sprite.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+class Player {
+public:
+	// コンストラクタ
+	Player();
+	// デストラクタ
+	~Player();
+
+	// 初期化
+	void Initialize(ViewProjection viewProjection);
+	// 更新
+	void Update();
+	// 3D描画
+	void Draw3D();
+	// インプットクラス
+	Input* input_ = nullptr;
+	float GetX() { return worldTransformPlayer_.translation_.x; }
+	float GetY() { return worldTransformPlayer_.translation_.y; }
+	float GetZ() { return worldTransformPlayer_.translation_.z; }
+
+private:
+	ViewProjection viewProjection_;
+	// プレイヤー
+	uint32_t textureHandlePlayer_ = 0;
+	Model* modelPlayer_ = nullptr;
+	WorldTransform worldTransformPlayer_;
+};
